14fdc1869d10bd2a3dcb0c7872721a3a19fc91dc
Sylvain Munaut <tnt@246tNt.com>
2022-08-28 00:07:39 +0200

    Initial verilog skeleton
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

---
+++ src/user_module_341174480471589458.v
@@ -0,0 +1,14 @@
+/*
+ * user_module_341174480471589458.v
+ */
+
+`default_nettype none
+
+module user_module_341174480471589458 (
+	input  wire  [7:0] io_in,
+	output wire  [7:0] io_out
+);
+
+	assign io_out = io_in;
+
+endmodule // user_module_341174480471589458

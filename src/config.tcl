339ad3f3d2c20ec24a85a501561aaea90672da68
Sylvain Munaut <tnt@246tNt.com>
2022-08-30 09:09:14 +0200

    cfg: Disable design opt resizer
    This messes with the ring oscillator delay chain
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/config.tcl
+++ src/config.tcl
@@ -48,3 +48,7 @@ set ::env(RT_MAX_LAYER) {met4}
 # connect to first digital rails
 set ::env(VDD_NETS) [list {vccd1}]
 set ::env(GND_NETS) [list {vssd1}]
+
+# disable resizing
+set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) {0}
+#set ::env(PL_RESIZER_TIMING_OPTIMIZATIONS) {0}

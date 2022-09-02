255534f98dc3b71fdfeb9aca30546fdbe7122f5c
Sylvain Munaut <tnt@246tNt.com>
2022-09-02 11:37:01 +0200

    rtl: Buffer input signals manually since resizer was disabled
    This yields the same final input structure as if the resizer was
    enabled
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- src/scan_wrapper_341174480471589458.v
+++ src/scan_wrapper_341174480471589458.v
@@ -34,6 +34,21 @@ module scan_wrapper_341174480471589458 (
         .X          (clk)
     );
 
+		// We disable the resizer, so buffer input like it is done
+		// when the resizer does it for us
+    wire data_in_i;
+    wire scan_select_in_i;
+    wire latch_enable_in_i;
+
+    sky130_fd_sc_hd__buf_2 input_buffers[2:0] (
+`ifdef WITH_POWER
+        .VPWR       (1'b1),
+        .VGND       (1'b0),
+`endif
+        .A          ({data_in,   scan_select_in,   latch_enable_in   }),
+        .X          ({data_in_i, scan_select_in_i, latch_enable_in_i })
+    );
+
     // output buffers
         // Same as for input, to try and be more consistent, we make our own
     wire data_out_i;
@@ -43,8 +58,8 @@ module scan_wrapper_341174480471589458 (
         .VPWR       (1'b1),
         .VGND       (1'b0),
 `endif
-        .A          ({clk,     data_out_i, scan_select_in,  latch_enable_in }),
-        .X          ({clk_out, data_out,   scan_select_out, latch_enable_out })
+        .A          ({clk,     data_out_i, scan_select_in_i, latch_enable_in_i }),
+        .X          ({clk_out, data_out,   scan_select_out,  latch_enable_out  })
     );
 
     /*
@@ -62,12 +77,11 @@ module scan_wrapper_341174480471589458 (
     // wires needed
     wire [NUM_IOS-1:0] scan_data_out;   // output of the each scan chain flop
     wire [NUM_IOS-1:0] scan_data_in;    // input of each scan chain flop
-    wire               module_latch_in; // buffered version of latch_enable_in
     wire [NUM_IOS-1:0] module_data_in;  // the data that enters the user module
     wire [NUM_IOS-1:0] module_data_out; // the data from the user module
 
     // scan chain - link all the flops, with data coming from data_in
-    assign scan_data_in = {scan_data_out[NUM_IOS-2:0], data_in};
+    assign scan_data_in = {scan_data_out[NUM_IOS-2:0], data_in_i};
 
     // end of the chain is a negedge FF to increase hold margin between blocks
     sky130_fd_sc_hd__dfrtn_1 out_flop (
@@ -93,7 +107,7 @@ module scan_wrapper_341174480471589458 (
         .CLK        (clk),
         .D          (scan_data_in),
         .SCD        (module_data_out),
-        .SCE        (scan_select_in),
+        .SCE        (scan_select_in_i),
         .Q          (scan_data_out)
     );
     `endif
@@ -102,19 +116,9 @@ module scan_wrapper_341174480471589458 (
     // Send output of scan chain directly
     assign module_data_in = scan_data_out;
 
-    // Buffer latch signal
-    sky130_fd_sc_hd__buf_2 latch_buf (
-`ifdef WITH_POWER
-        .VPWR       (1'b1),
-        .VGND       (1'b0),
-`endif
-        .A          (latch_enable_in),
-        .X          (module_latch_in)
-    );
-
     // instantiate the wokwi module
     user_module_341174480471589458 user_module(
-        .latch_in  (module_latch_in),
+        .latch_in  (latch_enable_in_i),
         .io_in     (module_data_in),
         .io_out    (module_data_out)
     );
